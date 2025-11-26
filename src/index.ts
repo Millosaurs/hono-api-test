d907e7a62ef412ce33bdc61315abba6cbe9aabab
Millosaurs <millosaurs@gmail.com>
2025-11-26 14:02:55 +0000

    final?

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import app from "@/app";
 import env from "@/env";
 
 serve(
-    {
-        fetch: app.fetch,
-        port: env.PORT,
-    },
-    (info) => {
-        // eslint-disable-next-line no-console
-        console.log(`Server is running on http://localhost:${info.port}`);
-    },
+  {
+    fetch: app.fetch,
+    port: env.PORT,
+  },
+  (info) => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
 );
+
+export default app;
